561ae8ae11e9bad8dbd15f66671e95390738bb25
iqra <im4356927@gmail.com>
2024-09-20 15:41:56 -0700

    third

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ io.on("connection", (socket) => {
     socket.emit("user-disconnection",socket.id)
 })
 
-server.listen(3000, () => {
-    console.log(`Server is running on port 3000 `);
+const PORT = process.env.PORT || 3000;
 
-})
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
