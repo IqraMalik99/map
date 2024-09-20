53b83f11339442a02fe93fbc39a51164c6473787
iqra <im4356927@gmail.com>
2024-09-20 15:45:11 -0700

    four

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
